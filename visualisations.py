9daeb3ec5ca775e3d4963055a6d3b151cb39aacf
Robert Edwin Rouse <robert.edwin.rouse@gmail.com>
2024-08-19 12:18:35 +0100

    Updated visualisations

--- visualisations.py
+++ visualisations.py
@@ -8,8 +8,10 @@ lethal or nonlethal, using a precompiled database.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtk
 import seaborn as sn
 import cartopy.crs as ccrs
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 from apollo import mechanics as ma
 
 
@@ -32,11 +34,11 @@ labels = ['Max T','Mean RH','Mean WS','30 ∆T','90 ∆T',
 ### Feature correlation heat map
 corr_df = df[features]
 corr_df.columns = labels
-corrMatrix = corr_df.corr()
-corrMatrix = corrMatrix.round(decimals=2)
-mask = np.triu(np.ones_like(corrMatrix.corr()), k=1)
+CM = corr_df.corr()
+CM = CM.round(decimals=2)
+mask = np.triu(np.ones_like(CM.corr()), k=1)
 fig, ax = plt.subplots(figsize=(16, 12))
-sn.heatmap(corrMatrix, vmin= -0.6, vmax=1.01, annot=True, cmap='flare', mask=mask)
+sn.heatmap(CM, vmin= -0.6, vmax=1.01, annot=True, cmap='flare', mask=mask)
 ax.set_xticklabels(labels, rotation=-45, ha='left')
 plt.show()
 
@@ -52,63 +54,41 @@ df['False +'] = (df['Documented'] == 0) & (df['Predicted'] > 0).astype(int)
 df['False -'] = (df['Documented'] > 0) & (df['Predicted'] == 0).astype(int)
 retain = ['Latitude','Longitude','Observed +','Observed -',
           'Predicted +','Predicted -','True +','True -','False +','False -']
-df_compressed = df[retain].groupby(retain[0:2],as_index=False).sum()
+df_bubble = df[retain].groupby(retain[0:2],as_index=False).sum()
 
 
-def dotogram(xf, data, lons, lats, colours, scaling, num):
-    fig, ax = plt.subplots(figsize=(12, 8))
-    ax = plt.subplot(projection = ccrs.PlateCarree())
-    ax.coastlines(resolution='50m', alpha=0.5)
-    ax.gridlines(draw_labels=False)
-    ax.set_xlim(lons[0], lons[1])
-    ax.set_ylim(lats[0], lats[1])
+### Regional bubble plots
+def heat_bubble(df, data, lons, lats, colours, scaling, num):
+    proj = ccrs.PlateCarree()    
+    fig, ax = plt.subplots(figsize=(12, 8), subplot_kw={'projection':proj})
     f = lambda x: (x**scaling[0])*scaling[1]
     g = lambda y: (y/scaling[1])**(1/scaling[0])
-    sc = ax.scatter(xf['Longitude'], xf['Latitude'], s=f(xf[data]),
+    sc = ax.scatter(df['Longitude'], df['Latitude'], s=f(df[data]),
                     c=colours[0], alpha=0.65, edgecolors=colours[1], lw=2.5)
-    ax.legend(*sc.legend_elements("sizes", num=num, func=g), labelspacing=1.2,
-              loc='upper left')
+    ax.set_extent([lons[0], lons[1], lats[0], lats[1]], proj)  
+    ax.coastlines(resolution='50m', alpha=0.5)
+    gl = ax.gridlines(crs=proj, draw_labels=True, )
+    gl.xformatter = LongitudeFormatter()
+    gl.yformatter = LatitudeFormatter()
+    gl.xlocator = mtk.MaxNLocator(5)
+    gl.ylocator = mtk.MaxNLocator(5)
+    gl.top_labels = False
+    gl.right_labels = False
+    ax.legend(*sc.legend_elements("sizes", num=num, func=g), labelspacing=2)
     plt.show()
 
+cset = [['indianred','crimson'],['cadetblue','steelblue']]
 
-dotogram(df_compressed, 'Observed +', [-20, 40], [30, 70],
-                  ['indianred','crimson'], [2,20], 4)
-dotogram(df_compressed, 'Predicted +', [-20, 40], [30, 70],
-                  ['indianred','crimson'], [2,20], 5)
-dotogram(df_compressed, 'False -', [-20, 40], [30, 70],
-                  ['indianred','crimson'], [2,20], 5)
+heat_bubble(df_bubble, 'Observed +', [-20, 40], [30, 70], cset[0], [2,10], 5)
+heat_bubble(df_bubble, 'Predicted +', [-20, 40], [30, 70], cset[0], [2,10], 6)
+heat_bubble(df_bubble, 'False -', [-20, 40], [30, 70], cset[0], [2,10], 6)
+heat_bubble(df_bubble, 'Observed -', [-20, 40], [30, 70], cset[1], [2,0.005], 5)
+heat_bubble(df_bubble, 'Predicted -', [-20, 40], [30, 70], cset[1], [2,0.005], 5)
+heat_bubble(df_bubble, 'False +', [-20, 40], [30, 70], cset[1], [2,10], 5)
 
-dotogram(df_compressed, 'Observed -', [-20, 40], [30, 70],
-                  ['cadetblue','steelblue'], [2,0.025], 5)
-dotogram(df_compressed, 'Predicted -', [-20, 40], [30, 70],
-                  ['cadetblue','steelblue'], [2,0.025], 5)
-dotogram(df_compressed, 'False +', [-20, 40], [30, 70],
-                  ['cadetblue','steelblue'], [2,10], 5)
-
-dotogram(df_compressed, 'Observed +', [60, 180], [0, 80],
-                  ['indianred','crimson'], [2,10], 4)
-dotogram(df_compressed, 'Predicted +', [60, 180], [0, 80],
-                  ['indianred','crimson'], [2,10], 5)
-dotogram(df_compressed, 'False -', [60, 180], [0, 80],
-                  ['indianred','crimson'], [2,10], 5)
-
-dotogram(df_compressed, 'Observed -', [60, 180], [0, 80],
-                  ['cadetblue','steelblue'], [2,0.025], 5)
-dotogram(df_compressed, 'Predicted -', [60, 180], [0, 80],
-                  ['cadetblue','steelblue'], [2,0.025], 5)
-dotogram(df_compressed, 'False +', [60, 180], [0, 80],
-                  ['cadetblue','steelblue'], [2,10], 5)
-
-dotogram(df_compressed, 'Observed +', [-170, -35], [0, 90],
-                  ['indianred','crimson'], [2,10], 4)
-dotogram(df_compressed, 'Predicted +', [-170, -35], [0, 90],
-                  ['indianred','crimson'], [2,10], 4)
-dotogram(df_compressed, 'False -', [-170, -35], [0, 90],
-                  ['indianred','crimson'], [2,10], 5)
-
-dotogram(df_compressed, 'Observed -', [-170, -35], [0, 90],
-                  ['cadetblue','steelblue'], [2,0.025])
-dotogram(df_compressed, 'Predicted -', [-170, -35], [0, 90],
-                  ['cadetblue','steelblue'], [2,0.025])
-dotogram(df_compressed, 'False +', [-170, -35], [0, 90],
-                  ['cadetblue','steelblue'], [2,10])
+heat_bubble(df_bubble, 'Observed +', [60, 180], [0, 80], cset[0], [2,10], 5)
+heat_bubble(df_bubble, 'Predicted +', [60, 180], [0, 80], cset[0], [2,10], 6)
+heat_bubble(df_bubble, 'False -', [60, 180], [0, 80], cset[0], [2,10], 5)
+heat_bubble(df_bubble, 'Observed -', [60, 180], [0, 80], cset[1], [2,0.01], 5)
+heat_bubble(df_bubble, 'Predicted -', [60, 180], [0, 80], cset[1], [2,0.01], 5)
+heat_bubble(df_bubble, 'False +', [60, 180], [0, 80], cset[1], [2,10], 5)
